9864ea2e0374f8155ee3ddc4db508246db261ab3
gbeltrame <beltramegustavo87@gmail.com>
2022-03-28 02:24:27 -0300

    Added Marketplace solidity contract

---
+++ src/backend/contracts/Marketplace.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+// Import contract standards (ERC 721)
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Marketplace is ReentrancyGuard {
+    // State Variable
+    address payable public immutable feeAccount; //the account that receives fees
+    uint public immutable feePercent; //the fee percentage on sales
+    uint public itemCount;
+
+    constructor(uint _feePercent){
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+    }
+
+}
--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -11,10 +11,15 @@ async function main() {
   const NFT = await ethers.getContractFactory("NFT");
   const nft = await NFT.deploy();
 
-  console.log("NFT Contract address: ", nft.address)
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  const marketplace = await Marketplace.deploy(1);
+
+  console.log("NFT Contract address: ", nft.address);
+  console.log("Marketplace Contract address: ", marketplace.address);
   
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
   saveFrontendFiles(nft, "NFT");
+  saveFrontendFiles(marketplace, "Marketplace");
 }
 
 function saveFrontendFiles(contract, name) {
